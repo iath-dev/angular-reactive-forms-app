fa99ce45284710572a80038397a84d66980f044a
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-07-29 15:00:18 -0500

    feat: basic page and dynamic page completed

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,30 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)], []],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  onSave() {
+    console.log(this.myForm.value);
+  }
+}
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,53 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
-  selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DynamicPageComponent {}
+export class DynamicPageComponent {
+  private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  public myForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.formBuilder.array(
+      [],
+      [Validators.required, Validators.minLength(1)]
+    ),
+  });
+
+  newFavorite = this.formBuilder.control('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  addFavorite() {
+    if (this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    this.favorites.push(this.formBuilder.control(newGame, Validators.required));
+
+    this.newFavorite.reset();
+  }
+
+  removeFavorite(index: number) {
+    this.favorites.removeAt(index);
+  }
+
+  get favorites() {
+    return this.myForm.get('favorites') as FormArray;
+  }
+
+  onSave() {
+    console.log(this.myForm.value);
+  }
+}
